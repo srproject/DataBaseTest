64e5c8683cff4c0f5bcf0f8021976a9f8f265ddc
sr <SR>
2017-03-11 20:50:02 +0200

    dry

--- app/src/main/java/com/example/mido/databasetest/MainActivity.java
+++ app/src/main/java/com/example/mido/databasetest/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.mido.databasetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
   DatabaseHelper myDB;
@@ -10,5 +18,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDB = new DatabaseHelper(this);
+
+        copyDatabase(getApplicationContext(),"Student.db");
+
+    }
+
+
+
+    public static void copyDatabase(Context c, String DATABASE_NAME) {
+        String databasePath = c.getDatabasePath(DATABASE_NAME).getPath();
+        File f = new File(databasePath);
+        OutputStream myOutput = null;
+        InputStream myInput = null;
+        Log.d("testing", " testing db path " + databasePath);
+        Log.d("testing", " testing db exist " + f.exists());
+
+        if (f.exists()) {
+            try {
+
+                File directory = new File("/mnt/sdcard/DB_SR");
+                if (!directory.exists())
+                    directory.mkdir();
+
+                myOutput = new FileOutputStream(directory.getAbsolutePath()
+                        + "/" + DATABASE_NAME);
+                myInput = new FileInputStream(databasePath);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = myInput.read(buffer)) > 0) {
+                    myOutput.write(buffer, 0, length);
+                }
+
+                myOutput.flush();
+            } catch (Exception e) {
+            } finally {
+                try {
+                    if (myOutput != null) {
+                        myOutput.close();
+                        myOutput = null;
+                    }
+                    if (myInput != null) {
+                        myInput.close();
+                        myInput = null;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }
     }
 }
